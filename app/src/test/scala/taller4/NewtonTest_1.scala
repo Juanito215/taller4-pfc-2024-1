be4c397a8304a50d4379ffda4e4c80d09908ddba
Juanito215 <juan.david.cuellar@correounivalle.edu.co>
2024-05-31 23:07:56 -0500

    Test de mostrar secuencial

--- app/src/test/scala/taller4/NewtonTest_1.scala
+++ app/src/test/scala/taller4/NewtonTest_1.scala
@@ -16,8 +16,24 @@ import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class NewtonTest_1 extends AnyFunSuite{
-  val objnewtontest1 = new Newton()
-  test("Newtontest1"){
-    //assert(objnewtontest1.mostrar(objnewtontest1.Expo(objnewtontest1.Atomo('x'), objnewtontest1.Numero(2.0)) == ))
+  val e = new Newton()
+  test("Expresion1"){
+    assert(e.mostrar(e.Expo(e.Atomo('x'), e.Numero(2.0))) == "(x ^ 2.0)")
   }
+  test("Expresion2"){
+    assert(e.mostrar(e.Prod(e.Prod(e.Numero(2.0), e.Expo(e.Atomo('x'), e.Numero(2.0))),e.Suma(e.Atomo('x'), e.Prod(e.Numero(4.0), e.Atomo('x'))))) ==
+            "((2.0 * (x ^ 2.0)) * (x + (4.0 * x)))")
+    }
+  test("Espresion3"){
+    assert(e.mostrar(e.Resta(e.Div(e.Numero(1.0), e.Atomo('x')),e.Div(e.Numero(1.0), e.Prod(e.Numero(2.0),e.Atomo('y'))))) ==
+      "((1.0 / x) - (1.0 / (2.0 * y)))")
+  }
+  test("Espresion4"){
+    assert(e.mostrar(e.Suma(e.Logaritmo(e.Prod(e.Numero(2.0), e.Atomo('x'))), e.Logaritmo(e.Prod(e.Numero(3.0), e.Atomo('x'))))) ==
+      "((ln((2.0 * x))) + (ln((3.0 * x))))")
+  }
+  test("Expresion5"){
+    assert(e.mostrar(e.Logaritmo(e.Expo(e.Atomo('e'), e.Prod(e.Numero(4.0), e.Atomo('x'))))) == "(ln((e ^ (4.0 * x))))")
+  }
+
 }
