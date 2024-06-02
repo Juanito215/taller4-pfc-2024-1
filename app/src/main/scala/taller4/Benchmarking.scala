fb6226a5d9bece439b4cd9358b52319d585d0266
joseph <joseph.herrera@correounivalle.edu.co>
2024-06-02 00:40:58 -0500

    Se agrega el NewtonParalela a las pruebas de Benchmarking.scala y se agrega un comentario.

--- app/src/main/scala/taller4/Benchmarking.scala
+++ app/src/main/scala/taller4/Benchmarking.scala
@@ -9,38 +9,37 @@ object Benchmarking {
 
 
   def runNewtonBenchmark(): Unit = {
-    val expr = n.Resta(n.Prod(n.Atomo('x'), n.Atomo('x')), n.Numero(2)) //Cambiar por expresion mas compleja.
+    val expr = n.Div(n.Prod(n.Atomo('x'), n.Resta(n.Numero(5), n.Atomo('y'))), n.Suma(n.Atomo('z'), n.Numero(2)))
     val atom = n.Atomo('x')
-    val initialValue = 1.0
+    val initialValue = 100.0
 
     val time = config(
       Key.exec.minWarmupRuns := 10,
-      Key.exec.maxWarmupRuns := 20,
-      Key.exec.benchRuns := 10,
-      Key.verbose := true
+      Key.exec.maxWarmupRuns := 10,
+      Key.exec.benchRuns := 20,
+      Key.verbose := true,
     ) withWarmer new Warmer.Default measure {
-      n.raizNewton(expr, atom, initialValue, n.buenaAprox)
+      n.raizNewton(expr, atom ,initialValue, n.buenaAprox)
     }
 
     println(s"Tiempo promedio de ejecución de raizNewton en Newton: $time")
   }
 
- /*
+
   def runNewtonParaleloBenchmark(): Unit = {
-    val expr = np.Resta(np.Prod(np.Atomo('x'), np.Atomo('x')), np.Numero(2))
+    val expr = np.Logaritmo(np.Prod(np.Suma(np.Atomo('x'), np.Numero(2)), np.Div(np.Expo(np.Atomo('x'), np.Numero(3)), np.Resta(np.Numero(7), np.Atomo('x')))))
     val atom = np.Atomo('x')
-    val initialValue = 1.0
+    val initialValue = 100.0
 
     val time = config(
       Key.exec.minWarmupRuns := 10,
       Key.exec.maxWarmupRuns := 10,
-      Key.exec.benchRuns := 10,
-      Key.verbose := true,
-      Key.exec.benchRuns := 10
-    ) withWarmer (new Warmer.Default) measure {
-      new NewtonParalela().raizNewton(expr, atom, initialValue, new NewtonParalela().buenaAprox)
+      Key.exec.benchRuns := 20,
+      Key.verbose := true
+    ) withWarmer new Warmer.Default measure {
+      np.raizNewton(expr, atom, initialValue, np.buenaAprox)
     }
 
     println(s"Tiempo promedio de ejecución de raizNewton en NewtonParalela: $time")
-  }*/
+  }
 }
--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -5,12 +5,8 @@
   */
 package taller4
 
-import org.scalameter.measure
 import org.scalameter.withWarmer
 import org.scalameter.Warmer
-import org.scalameter._
-import taller4.Benchmarking
-
 
 object Taller4{
 
@@ -25,8 +21,9 @@ object Taller4{
 
     )
   }
-  println("Ejecutando pruebas de rendimiento desde Taller4.scala")
+  println("Ejecutando pruebas de rendimiento desde Taller4.scala\n\n") //NO ejecuta expresiones algebraicas muy complejas, se demora minimo 20 minutos.
   Benchmarking.runNewtonBenchmark()
-  println("Ejecutando pruebas de rendimiento en paralelo desde Taller4.scala")
-  //Benchmarking.runNewtonParaleloBenchmark()
+
+  println("Ejecutando pruebas de rendimiento en paralelo desde Taller4.scala") //Ejecuta all tipo de expresiones algebraicas.
+  Benchmarking.runNewtonParaleloBenchmark()
  }
